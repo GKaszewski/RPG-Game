37b8ddaeb3c805b8b4d6356a4487a1ce92f3fc26
GKaszewski <gabrielkaszewski@gmail.com>
2018-12-20 23:08:05 +0100

    project init

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    private float _yaw;
+    private float _pitch;
+    private Vector3 _rotationSmoothVelocity;
+    private Vector3 _currentRotation;
+    public Transform target;
+    public bool isPlayerTarget = true;
+    public float mouseSensitivity = 10f;
+    public float distanceFromPlayer = 4f;
+    public float rotationSmoothness = .12f;
+    public Vector2 minMaxPitch;
+    public Vector2 minMaxYaw;
+
+    private void Awake()
+    {
+        if(isPlayerTarget)
+            target = GameObject.FindWithTag("Player").transform;
+    }
+
+    private void Update()
+    {
+        _yaw += Input.GetAxis("Mouse X") * mouseSensitivity;
+        _pitch -= Input.GetAxis("Mouse Y") * mouseSensitivity;
+        _yaw = Mathf.Clamp(_yaw, minMaxYaw.x, minMaxYaw.y);
+        _pitch = Mathf.Clamp(_pitch, minMaxPitch.x, minMaxPitch.y);
+
+        _currentRotation = Vector3.SmoothDamp(_currentRotation, new Vector3(_pitch, _yaw), ref _rotationSmoothVelocity, rotationSmoothness);
+        transform.eulerAngles = _currentRotation;
+    }
+
+    private void LateUpdate() => transform.position = target.position - transform.forward * distanceFromPlayer;
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Animator _anim;
+    private CharacterController _characterController;
+    private Transform _cameraTransform;
+    private Vector3 _walkAcceleration;
+    private float _jumpAcceleration;
+    public float walkSpeed = 5f;
+    public float walkHorizontalSpeed = 4f;
+    public float walkBackwardSpeed = 3f;
+    public float rotateSpeed = 100f;
+    public float jumpStrength = 0.5f;
+
+    private void Awake()
+    {
+        _characterController = GetComponent<CharacterController>();
+        _cameraTransform = GameObject.FindWithTag("MainCamera").transform;
+
+        if(GetComponent<Animator>() != null)
+            _anim = GetComponent<Animator>();        
+    }
+
+    private void Update()
+    {
+        TakeKeyboardInput();
+        DoMovement();
+        CalculateAnimator();
+    }
+
+    private void TakeKeyboardInput()
+    {
+        if(Input.GetKey(KeyCode.W))
+        {
+            if(_characterController.isGrounded)
+                _walkAcceleration += transform.forward * walkSpeed;
+            else
+                _walkAcceleration = transform.forward * walkSpeed;    
+        }
+
+        if(Input.GetKey(KeyCode.S))
+        {
+            if(_characterController.isGrounded)
+                _walkAcceleration += transform.forward * -walkBackwardSpeed;
+            else
+                _walkAcceleration = transform.forward * -walkBackwardSpeed;    
+        }
+
+        if(Input.GetKey(KeyCode.A))
+        {
+            if(_characterController.isGrounded)
+                _walkAcceleration += transform.right * -walkHorizontalSpeed;
+            else
+                _walkAcceleration = transform.right * -walkHorizontalSpeed;    
+        }
+
+        if(Input.GetKey(KeyCode.D))
+        {
+            if(_characterController.isGrounded)
+                _walkAcceleration += transform.right * walkHorizontalSpeed;
+            else
+                _walkAcceleration = transform.right * walkHorizontalSpeed;    
+        }
+
+        if(Input.GetKey(KeyCode.Space))
+        {
+            _jumpAcceleration = jumpStrength;
+        }
+    }
+
+    private void DoMovement()
+    {
+        _characterController.Move(_walkAcceleration * Time.deltaTime);
+        _characterController.Move(new Vector3(0, _jumpAcceleration, 0));
+        transform.eulerAngles = new Vector3(0, _cameraTransform.eulerAngles.y, 0);
+
+        if (_characterController.isGrounded)
+            _walkAcceleration = Vector3.zero;
+        else
+            _walkAcceleration = Vector3.MoveTowards(_walkAcceleration, Vector3.zero, Time.deltaTime);
+
+        if(_jumpAcceleration > -0.98f)
+            _jumpAcceleration = Mathf.MoveTowards(_jumpAcceleration, -0.98f, Time.deltaTime * 2f);    
+    }
+    
+    private void CalculateAnimator()
+    {
+        
+    }
+}
